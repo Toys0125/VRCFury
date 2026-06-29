ef976754e17bc5fbf21e723f212fc742c19dc876
Toys0125 <marcus.g.stange@gmail.com>
2026-06-29 19:24:22 +0000

    Expect hook iterator exception log in test

--- com.vrcfury.vrcfury/Tests/FuryArmatureLinkHooksTests.cs
+++ com.vrcfury.vrcfury/Tests/FuryArmatureLinkHooksTests.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using com.vrcfury.api.Integration;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.TestTools;
 
 [Category("VRCFury")]
 public class FuryArmatureLinkHooksTests {
@@ -12,6 +14,7 @@ public class FuryArmatureLinkHooksTests {
         var root = new GameObject("VRCFury Hook Test Avatar");
         FuryArmatureLinkHooks.CollectArmatureLinks += ThrowingIteratorCollector;
         try {
+            LogAssert.Expect(LogType.Exception, new Regex("VRCFury armature link hook collector failed while enumerating requests"));
             var requests = FuryArmatureLinkHooks.InvokeCollectors(root);
             Assert.That(requests.Count(r => r.source == "throwing iterator test"), Is.EqualTo(1));
         } finally {
